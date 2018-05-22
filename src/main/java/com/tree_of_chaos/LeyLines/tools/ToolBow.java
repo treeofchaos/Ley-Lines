bdce53ad434edf6ebe2e3537a61fbb9503d845b9
treeofchaos <39061897+treeofchaos@users.noreply.github.com>
2018-05-22 15:58:41 -0400

    Add files via upload

--- src/main/java/com/tree_of_chaos/LeyLines/tools/ToolBow.java
+++ src/main/java/com/tree_of_chaos/LeyLines/tools/ToolBow.java
@@ -3,15 +3,22 @@ import com.tree_of_chaos.LeyLines.Main;
 import com.tree_of_chaos.LeyLines.init.ModItems;
 import com.tree_of_chaos.LeyLines.util.IHasModel;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemArrow;
 import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 public class ToolBow extends ItemBow implements IHasModel 
 {
 	public ToolBow(String name, ToolMaterial material)
 	{
 		setUnlocalizedName(name);
 		setRegistryName(name);
-		setCreativeTab(CreativeTabs.MATERIALS);
+		setCreativeTab(CreativeTabs.COMBAT);
 		ModItems.ITEMS.add(this);
 		
 	}
@@ -19,4 +26,51 @@ public class ToolBow extends ItemBow implements IHasModel
 	{
 		Main.proxy.registerItemRenderer(this,0,"inventory");
 	}
+    private ItemStack findAmmo(EntityPlayer player)
+    {
+        if (this.isArrow(player.getHeldItem(EnumHand.OFF_HAND)))
+        {
+            return player.getHeldItem(EnumHand.OFF_HAND);
+        }
+        else if (this.isArrow(player.getHeldItem(EnumHand.MAIN_HAND)))
+        {
+            return player.getHeldItem(EnumHand.MAIN_HAND);
+        }
+        else
+        {
+            for (int i = 0; i < player.inventory.getSizeInventory(); ++i)
+            {
+                ItemStack itemstack = player.inventory.getStackInSlot(i);
+
+                if (this.isArrow(itemstack))
+                {
+                    return itemstack;
+                }
+            }
+
+            return ItemStack.EMPTY;
+        }
+    }
+    protected boolean isArrow(ItemStack stack)
+    {
+        return stack.getItem() instanceof ToolArrow;
+    }
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+        boolean flag = !this.findAmmo(playerIn).isEmpty();
+
+        ActionResult<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onArrowNock(itemstack, worldIn, playerIn, handIn, flag);
+        if (ret != null) return ret;
+
+        if (!playerIn.capabilities.isCreativeMode && !flag)
+        {
+            return flag ? new ActionResult(EnumActionResult.PASS, itemstack) : new ActionResult(EnumActionResult.FAIL, itemstack);
+        }
+        else
+        {
+            playerIn.setActiveHand(handIn);
+            return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
+        }
+    }
 }
