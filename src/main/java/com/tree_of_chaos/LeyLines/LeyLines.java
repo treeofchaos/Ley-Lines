8ceb385a2ccc765f50949e460ffe8a317e9e58e5
treeofchaos <39061897+treeofchaos@users.noreply.github.com>
2018-08-23 13:44:55 -0400

    Added Config File
    Added a config file

---
+++ src/main/java/com/tree_of_chaos/LeyLines/LeyLines.java
@@ -0,0 +1,43 @@
+package com.tree_of_chaos.LeyLines;
+
+import org.apache.logging.log4j.Logger;
+
+import com.tree_of_chaos.LeyLines.proxy.CommonProxy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = LeyLines.MODID, name = LeyLines.MODNAME, version = LeyLines.MODVERSION, dependencies = "required-after:Forge@[14.23.3.2655,)", useMetadata = true)
+public class LeyLines {
+
+    public static final String MODID = "leylines";
+    public static final String MODNAME = "LeyLines";
+    public static final String MODVERSION = "0.0.1";
+
+    @SidedProxy(clientSide = "tree_of_chaos.LeyLines.proxy.ClientProxy", serverSide = "tree_of_chaos.LeyLinesproxy.ServerProxy")
+    public static CommonProxy proxy;
+
+    @Mod.Instance
+    public static LeyLines instance;
+
+    public static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e) {
+        proxy.init(e);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+        proxy.postInit(e);
+    }
+}
--- src/main/java/com/tree_of_chaos/LeyLines/proxy/CommonProxy.java
+++ src/main/java/com/tree_of_chaos/LeyLines/proxy/CommonProxy.java
@@ -1,6 +1,11 @@
 package com.tree_of_chaos.LeyLines.proxy;
+import java.io.File;
+
+import com.tree_of_chaos.LeyLines.util.Config;
+
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -9,30 +14,36 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber
 public class CommonProxy {
-    public void preInit(FMLPreInitializationEvent e) 
-    {
-    	
-    }
+	public void preInit(FMLPreInitializationEvent e) 
+	{
+		File directory = e.getModConfigurationDirectory();
+		config = new Configuration(new File(directory.getPath(), "leylines.cfg"));
+		Config.readConfig();
+	}
 
-    public void init(FMLInitializationEvent e) 
-    {
-    	
-    }
+	public void init(FMLInitializationEvent e) 
+	{
 
-    public void postInit(FMLPostInitializationEvent e) 
-    {
-    	
-    }
+	}
 
-    @SubscribeEvent
-    public static void registerBlocks(RegistryEvent.Register<Block> event) 
-    {
-    	
-    }
+	public void postInit(FMLPostInitializationEvent e) 
+	{
+        if (config.hasChanged()) 
+        {
+            config.save();
+        }
+	}
 
-    @SubscribeEvent
-    public static void registerItems(RegistryEvent.Register<Item> event) 
-    {
-    	
-    }
+	@SubscribeEvent
+	public static void registerBlocks(RegistryEvent.Register<Block> event) 
+	{
+
+	}
+
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event) 
+	{
+
+	}
+	public static Configuration config;
 }
