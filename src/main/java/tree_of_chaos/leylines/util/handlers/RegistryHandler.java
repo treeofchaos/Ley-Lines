baad867cec43757789cd4a625303389532781520
TheDutchMC04 <tobias64a@gmail.com>
2018-09-19 19:30:14 +0200

    updates

--- src/main/java/tree_of_chaos/leylines/util/handlers/RegistryHandler.java
+++ src/main/java/tree_of_chaos/leylines/util/handlers/RegistryHandler.java
@@ -1,8 +1,6 @@
 package tree_of_chaos.leylines.util.handlers;
 
 import tree_of_chaos.leylines.init.BlockInit;
-import tree_of_chaos.leylines.init.ItemInit;
-import tree_of_chaos.leylines.util.IHasModel;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -10,21 +8,43 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@EventBusSubscriber()
-public class RegistryHandler 
-{
-	public static void preInitRegistries() 
-	{
+@EventBusSubscriber
+public class RegistryHandler {
+
+	public static void preInitRegistries() {
 
     }
 
-    public static void initRegistries() 
-    {
+    public static void initRegistries() {
 
     }
 
-    public static void postInitRegistries() 
-    {
+    public static void postInitRegistries() {
     	
     }
+    
+    @SubscribeEvent
+    public static void onItemRegister(RegistryEvent.Register<Item> event) {
+    	
+       /* ItemInit.register(event.getRegistry()); */
+        BlockInit.registerItemBlocks(event.getRegistry());
+        
+    }
+    
+    @SubscribeEvent
+    public static void onBlockRegister(RegistryEvent.Register<Block> event) {
+    	
+        BlockInit.register(event.getRegistry());
+        
+    }
+
+    @SubscribeEvent
+    public static void onModelRegister(ModelRegistryEvent event) {
+    	
+      /*  ItemInit.registerModels(); */
+        BlockInit.registerModels();
+        
+    }
+
+	    
 }
